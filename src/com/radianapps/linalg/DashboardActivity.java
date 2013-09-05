cdb90fa454fc0d3817b0a2115fe2b6b976fefdbd
Tushar Dhoot <tushardhoot@live.com>
2013-09-04 23:12:14 -0400

    Initial project building

---
+++ src/com/radianapps/linalg/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.radianapps.linalg;
+
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tushar
+ * Date: 8/29/13
+ * Time: 1:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DashboardActivity extends SlidingActivity {
+
+
+
+}
